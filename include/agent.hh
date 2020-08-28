9364ba642a316562924398560a60bdeb070524ae
Stanisław Grams <sjg@fmdx.pl>
2020-08-28 23:09:18 +0200

    include: update API links (hydro.imgw.pl instead of monitor.pogodynka.pl)

--- include/agent.hh
+++ include/agent.hh
@@ -15,10 +15,10 @@
 #include "types.hh"
 #include "data.hh"
 
-#define AGENT_IDFETCHER_URI_PREFIX  "http://monitor.pogodynka.pl/api/search/?query="
+#define AGENT_IDFETCHER_URI_PREFIX  "https://hydro.imgw.pl/api/search/?query="
 #define AGENT_IDFETCHER_URI_SUFFIX  "&limit=10"
-#define AGENT_DATAFETCHER_METEO_URI "http://monitor.pogodynka.pl/api/station/meteo/?id="
-#define AGENT_DATAFETCHER_HYDRO_URI "http://monitor.pogodynka.pl/api/station/hydro/?id="
+#define AGENT_DATAFETCHER_METEO_URI "https://hydro.imgw.pl/api/station/meteo/?id="
+#define AGENT_DATAFETCHER_HYDRO_URI "https://hydro.imgw.pl/api/station/hydro/?id="
 #define AGENT_USERAGENT             "meteodata-agent/git"
 #define AGENT_TIMEOUT 10
 
